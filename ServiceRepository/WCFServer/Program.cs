f3e35e0467c60658151c69d0a01a6faf1c0939f7
mateuszs <mscirka@gmail.com>
2014-04-24 16:57:00 +0200

    14-04-24
    Podstawowa wersja IServiceRepository

---
+++ ServiceRepository/WCFServer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Runtime.Serialization;
+using NServiceRepository;
+
+/**
+ * ######## ServiceRepository #########
+ * 
+ * Authors: Mateusz Ścirka, Konrad Seweryn
+ * 
+ * */
+
+namespace NServiceRepository
+{ 
+    /**
+     * Klasa główna programu
+     * */
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ServiceRepository Repository = new ServiceRepository();
+                //Repository.Register("yoyo", "yyyy");
+
+                var Server = new ServiceRepositoryHost(Repository, "net.tcp://localhost:41234/IServiceRepository");
+                Server.AddDefaultEndpoint("net.tcp://localhost:41234/IServiceRepository");
+                Server.Open();
+
+                Console.WriteLine("Chyba działa...");
+            }
+            catch (ServiceRepositoryException Ex)
+            {
+                Console.WriteLine(Ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
