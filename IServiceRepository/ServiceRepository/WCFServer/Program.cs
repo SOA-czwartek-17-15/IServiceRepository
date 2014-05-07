a4a2e743658a216ea9f968186558dcf1e9a3c82b
mateuszs <mscirka@gmail.com>
2014-05-07 23:11:43 +0200

    Prawie gotowe
    Klient przykładowy

---
+++ IServiceRepository/ServiceRepository/WCFServer/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Runtime.Serialization;
+using NServiceRepository;
+using System.Configuration;
+using log4net;
+using System.Data.Entity;
+using WCFServer.Models;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+/**
+ * ######## ServiceRepository #########
+ * 
+ * Authors: Mateusz Ścirka, Konrad Seweryn
+ * 
+ * */
+
+namespace NServiceRepository
+{ 
+    /**
+     * Klasa główna programu
+     * */
+    public class Program
+    {
+        
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+            Database.SetInitializer<EFDbContext>(new DropCreateDatabaseIfModelChanges<EFDbContext>());
+            log4net.Config.XmlConfigurator.Configure();
+            try
+            {
+                ServiceRepository Repository = new ServiceRepository();
+                //Repository.Register("yoyo", "yyyy");
+                string serviceRepoAddress = ConfigurationSettings.AppSettings["serviceRepoAddress"];
+                var Server = new ServiceRepositoryHost(Repository, serviceRepoAddress);
+                Server.AddDefaultEndpoint("net.tcp://localhost:41234/IServiceRepository");
+                ServiceDebugBehavior debug = Server.Description.Behaviors.Find<ServiceDebugBehavior>();
+                // if not found - add behavior with setting turned on 
+                if (debug == null)
+                {
+                    Server.Description.Behaviors.Add(
+                         new ServiceDebugBehavior() { IncludeExceptionDetailInFaults = true });
+                }
+                else
+                {
+                    // make sure setting is turned ON
+                    if (!debug.IncludeExceptionDetailInFaults)
+                    {
+                        debug.IncludeExceptionDetailInFaults = true;
+                    }
+                }
+                Server.Open();
+                log.Info("Uruchomienie Serwera");
+                Console.WriteLine("Chyba działa...");
+            }
+            catch (ServiceRepositoryException Ex)
+            {
+                log.Info("Złapano wyjatek: " + Ex.Message);
+                Console.WriteLine(Ex.Message);
+            }
+            Console.ReadLine();
+            log.Info("Zatrzymanie Serwera");
+        }
+    }
+}
---
+++ IServiceRepository/ServiceRepository/WCFServer/ServiceRepository.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Runtime.Serialization;
+using log4net;
+using WCFServer.Models;
+using WCFServer;
+using System.Threading;
+
+namespace NServiceRepository
+{
+    /**
+     * Klasa modułu ServiceRepository
+     * */
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class ServiceRepository : IServiceRepository
+    {
+        /**
+         * Lista z serwisami
+         * */
+        private List<Service> Services;
+        private Repository Repo;
+        private MyTimer oTimer;
+            
+
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ServiceRepository()
+        {
+            Services = new List<Service>();
+            Repo = new Repository();
+            oTimer = new MyTimer(Repo);
+            Repo.CleanServices();
+            Thread timerThread = new Thread(oTimer.StartTimer);
+            timerThread.Start();
+        }
+
+        /**
+         *  Rejestrowanie serwisu
+         * */
+        public void RegisterService(String Name, String Address)
+        {
+            if (Name == String.Empty || Address == String.Empty)
+                throw new EmptyAddressOrNameException();
+
+            if (FindService(Name) != null)
+                throw new ServiceAlreadyExistsException();
+
+            var NewService = new Service();
+            NewService.Adress = Address;
+            NewService.Name = Name;
+            NewService.LastSeen = DateTime.Now;
+            Console.WriteLine("Zarejestrowano serwis: " + Name + " pod adresem: " + Address);
+            log.Info("Zarejestrowano serwis: " + Name + " pod adresem: " + Address);
+            Services.Add(NewService);
+            Repo.AddService(NewService);
+        }
+
+        /**
+         * Pobieranie adresu serwisu
+         * */
+        public String GetServiceLocation(String Name)
+        {
+            var Service = FindService(Name);
+
+            if (Service == null) throw new ServiceNotFoundException();
+
+            return Service.Adress;
+        }
+
+        /**
+         * Wyrejestrowywanie serwisu
+         * */
+        public void Unregister(String Name)
+        {
+            var Service = FindService(Name);
+            if (Service == null) throw new ServiceNotFoundException();
+            Console.WriteLine("Wyrejestrowano serwis: " + Name);
+            log.Info("Wyrejestrowano serwis: " + Name);
+            Services.Remove(Service);
+            Repo.RemoveService(Service);
+        }
+
+        /**
+         * Tu będzie odnowa połączenia
+         * */
+        public void Alive(String Name)
+        {
+            var Service = FindService(Name);
+            if (Service == null) throw new ServiceNotFoundException();
+            Service.LastSeen = DateTime.Now; 
+            Repo.UpdateService(Service);
+            Console.WriteLine("Zglosil sie serwis: "+Name);
+            log.Info("Zglosil sie serwis: " + Name );
+        }
+
+        /**
+         * Odszukiwanie serwisu w liście Services
+         * */
+        private Service FindService(String Name)
+        {
+            return Repo.FindService(Name);
+            //return Services.Find(Element => Element.Name == Name);
+        }
+
+    }
+}
